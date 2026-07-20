171d57476a13c058a6f46eacc7d67349cda69250
Muhammad Khan <m4studyonly@gmail.com>
2026-07-20 21:11:37 +0500

    removed unused import

--- lib/features/daily_logging/presentation/daily_entry_screen.dart
+++ lib/features/daily_logging/presentation/daily_entry_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:halaqat/features/progress_tracking/data/app_data.dart';
